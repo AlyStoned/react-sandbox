f7e3f1e0c15eb46ca3783de3588283854e39f193
Alexey <Aly8356@gmail.com>
2022-01-30 23:44:11 +0400

    test cross domain msgs

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,5 +1,5 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import counterReducer, {incrementByAmount} from '../features/counter/counterSlice';
 
 export const store = configureStore({
   reducer: {
@@ -15,3 +15,21 @@ export type AppThunk<ReturnType = void> = ThunkAction<
   unknown,
   Action<string>
 >;
+
+window.addEventListener('message', event => {
+    console.log(event);
+    console.log(event.data);
+    // IMPORTANT: check the origin of the data!
+    if (event.origin.startsWith('http://127.0.0.1:3001')) {
+    // if (event.origin.startsWith('http://your-first-site.com')) {
+        // The data was sent from your site.
+        // Data sent with postMessage is stored in event.data:
+        console.log(store);
+        store.dispatch(incrementByAmount(event.data))
+    } else {
+        // The data was NOT sent from your site!
+        // Be careful! Do not use it. This else branch is
+        // here just for clarity, you usually shouldn't need it.
+        return;
+    }
+});
